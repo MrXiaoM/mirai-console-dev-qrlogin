9c9b8e3cff81dafb20a07ce99d7f31b024a98857
ヒューマンワーキン <coolxiaom95@gmail.com>
2023-04-17 17:34:50 +0800

    exit on mirai 2.14.0

--- src/main/kotlin/QRLogin.kt
+++ src/main/kotlin/QRLogin.kt
@@ -9,6 +9,7 @@ import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 import net.mamoe.mirai.console.util.SemVersion
 import top.mrxiaom.qrlogin.commands.QRAutoLoginCommand
 import top.mrxiaom.qrlogin.commands.QRLoginCommand
+import kotlin.system.exitProcess
 
 object QRLogin : KotlinPlugin(
     JvmPluginDescription(
@@ -22,7 +23,7 @@ object QRLogin : KotlinPlugin(
     private val enable by lazy {
         logger.warning("本插件仅在 2.15.0 或以上工作，且在正式版或先行发布版(RC)中可能无法使用")
         if (SemVersion.parseRangeRequirement("<= 2.14.0").test(MiraiConsole.version)) {
-            return@lazy false
+            exitProcess(255)
         }
         return@lazy true
     }
